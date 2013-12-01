edc339f57db634e1008a2d336c2dbc936965ee9e
Christina Atallah <aphex.tinez@gmail.com>
2013-12-01 00:46:37 -0500

    Initial Commit

---
+++ Matchmaker/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  Matchmaker
+//
+//  Created by Atallah on 12/1/13.
+//  Copyright (c) 2013 Atallah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchmaker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchmaker
+//
+//  Created by Atallah on 12/1/13.
+//  Copyright (c) 2013 Atallah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
