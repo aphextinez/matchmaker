a40fbf60259eb6aaebf9c1a8de2de2c15d2ab309
Christina Atallah <aphex.tinez@gmail.com>
2013-12-02 19:37:47 -0500

    "Added server to Tina's storyboard stuff"

---
+++ Matchmaker/XYZUser.h
@@ -0,0 +1,13 @@
+//
+//  XYZUser.h
+//  Matchmaker
+//
+//  Created by Atallah on 12/2/13.
+//  Copyright (c) 2013 Atallah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYZUser : NSObject
+
+@end
---
+++ Matchmaker/XYZUser.m
@@ -0,0 +1,13 @@
+//
+//  XYZUser.m
+//  Matchmaker
+//
+//  Created by Atallah on 12/2/13.
+//  Copyright (c) 2013 Atallah. All rights reserved.
+//
+
+#import "XYZUser.h"
+
+@implementation XYZUser
+
+@end
